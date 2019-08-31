990a6aa1437526244cc78d7d235053aa3f9ac670
Michał Szczepańczyk <25662022+Shaid3r@users.noreply.github.com>
2019-08-31 16:45:52 +0200

    Fix settings tests

--- tests/test_analyser/test_analyser.py
+++ tests/test_analyser/test_analyser.py
@@ -1,19 +1,38 @@
-from decorators import settings
-from analyser import Analyser
+from decorators import settings, override_settings
+# from analyser import Analyser
 
 
 
-DUMMY_SETTINGS = {'magicWords' : {'dosłownie' : 1}}
+# DUMMY_SETTINGS = {'magicWords' : {'dosłownie' : 1}}
 
 
-@settings(DUMMY_SETTINGS)
-def test_shouldReturnCorrectList():
-    a = Analyser()
-    a.analyse("dosłownie dosłownie")
-    assert ["dosłownie"] == a.getFinalCommand()
+@override_settings({'commands': {'exit': '\ALT+\F4'}})
+def test_by_default_should_override_only_active_settings():
+    assert settings.active()['commands']['exit'] == '\ALT+\F4'
 
-@settings(DUMMY_SETTINGS)
-def test_shouldReturnCorrfdfsdfdsectList():
-    a = Analyser()
-    a.analyse("dosłownie dosłownie dosłownie dosłownie")
-    assert ["dosłownie", "dosłownie"] == a.getFinalCommand()
+
+@override_settings({
+        'general': {
+            'language': 'pl'
+        },
+        'default': {
+            'commands': {
+                'exit': '\ALT+\F4'
+            }
+        }
+    }, only_active=False)
+def test_when_only_active_should_override_whole_settings():
+    assert settings.active()['commands']['exit'] == '\ALT+\F4'
+    assert settings['general']['language'] == 'pl'
+
+# @override_settings(DUMMY_SETTINGS)
+# def test_shouldReturnCorrectList():
+#     a = Analyser()
+#     a.analyse("dosłownie dosłownie")
+#     assert ["dosłownie"] == a.getFinalCommand()
+
+# @override_settings(DUMMY_SETTINGS)
+# def test_shouldReturnCorrfdfsdfdsectList():
+#     a = Analyser()
+#     a.analyse("dosłownie dosłownie dosłownie dosłownie")
+#     assert ["dosłownie", "dosłownie"] == a.getFinalCommand()
