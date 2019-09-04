3e83e93a7d49f6a860b858d0e569c24af937e589
Kamil Warchoł <kamil.warchol@nokia.com>
2019-09-04 21:49:15 +0200

    Fix default config formatting

--- src/conf/default_config.py
+++ src/conf/default_config.py
@@ -1,55 +1,59 @@
+from pynput.keyboard import Key
+
+
 data = {
-    'keyboard_mapping' : {
-        'spacja' : ' ',
-        'cudzysłów' : '\"',
-        'tylda' : "~",
-        'wykrzyknik' : '!',
-        'małpa' : '@',
-        'kratka' : '#',
-        'dolar' : '$',
-        'procent' : '%',
-        'kareta' : '^',
-        'ampersand' : '&',
-        'gwiazdka' : '*',
-        'minus' : '-',
-        'podkreślenie' : '_',
-        'plus' : '+',
-        'równe' : '=',
-        'dwukropek' : ':',
-        'średnik' : ';',
-        'kropka' : '.',
-        'przecinek' : ',',
-        'ukośnik' : '/',
-        'apostrof' : '\'',
-        'jeden' : '1', # jeden, raz, jedynka - google nie ogarnia
-        '2' : 'dwa',
-        '3' :'trzy',
-        '4' : 'cztery',
-        '5' : 'pięć',
-        '6' : 'sześć',
-        '7' : 'siedem',
-        '8' : 'osiem',
-        '9' : 'dziewięć',
-        '10' : 'dziesięć',
-        '11' : 'jedenaście',
-        '12' : 'dwanaście',
-        '13' : 'trzynaście',
-        '14' : 'czternaście',
-        '15' : 'piętnaście',
-        '16' : 'szesnaście',
-        '17' : 'siedemnaście',
-        '18' : 'osiemnaście',
-        '19' : 'dziewiętnaście',
-        '20' : 'dwadzieścia' # TODO
+    'keyboard_mapping': {
+        'enter': Key.enter,
+        'spacja': Key.space,
+        'cudzysłów': '\"',
+        'tylda': "~",
+        'wykrzyknik': '!',
+        'małpa': '@',
+        'kratka': '#',
+        'dolar': '$',
+        'procent': '%',
+        'kareta': '^',
+        'ampersand': '&',
+        'gwiazdka': '*',
+        'minus': '-',
+        'podkreślenie': '_',
+        'plus': '+',
+        'równe': '=',
+        'dwukropek': ':',
+        'średnik': ';',
+        'kropka': '.',
+        'przecinek': ',',
+        'ukośnik': '/',
+        'apostrof': '\'',
+        'jeden': '1',  # jeden, raz, jedynka - google nie ogarnia
+        '2': 'dwa',
+        '3': 'trzy',
+        '4': 'cztery',
+        '5': 'pięć',
+        '6': 'sześć',
+        '7': 'siedem',
+        '8': 'osiem',
+        '9': 'dziewięć',
+        '10': 'dziesięć',
+        '11': 'jedenaście',
+        '12': 'dwanaście',
+        '13': 'trzynaście',
+        '14': 'czternaście',
+        '15': 'piętnaście',
+        '16': 'szesnaście',
+        '17': 'siedemnaście',
+        '18': 'osiemnaście',
+        '19': 'dziewiętnaście',
+        '20': 'dwadzieścia'  # TODO
 
     },
     'analysersettings': {
-        'autospace' : 'autospace',
-        'explicit' : 'dosłownie'
+        'autospace': 'autospace',
+        'explicit': 'dosłownie'
 
     },
     'servicemapping': {
-        'komenda' : 'CommandService'
+        'komenda': 'CommandService'
     },
     'general': {
         'language': 'pl',
@@ -60,8 +64,8 @@ data = {
     },
     'default': {
         'commands': {
-            'zamknij' : [('alt', 'f4')],
-            'vim zapisz i zamknij' : [':', 'wq', ('enter')]
+            'zamknij': [(Key.alt, Key.f4)],
+            'vim zapisz i zamknij': [':', 'wq', Key.enter]
         }
     },
     'terminal': {
